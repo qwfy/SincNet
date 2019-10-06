e625b8768c0e31d7d797119349c383c79c4d6337
Incomplete <incomplete@aixon.co>
2019-10-06 17:52:52 +0800

    Prototype of speaker_verification

--- speaker_verification.py
+++ speaker_verification.py
@@ -1,15 +1,18 @@
 # python speaker_verification.py --cfg=cfg/SincNet_TIMIT.cfg
 
+import collections
 import os
+import random
 import time
 
 import numpy as np
+import soundfile as sf
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import soundfile as sf
+from torch.utils.data import IterableDataset
+from torch.utils.data import DataLoader
 
-from data_io import ReadList
 from data_io import read_conf
 from data_io import str_to_bool
 from dnn_models import MLP
@@ -19,36 +22,87 @@ IS_DATA_PARALLEL = False
 DEVICE_IDS = [0]
 # IS_DATA_PARALLEL = True
 # DEVICE_IDS = list(range(8))
-device = torch.device(f"cuda:{DEVICE_IDS[0]}")
+device = torch.device(f'cuda:{DEVICE_IDS[0]}')
 
-def create_batches_rnd(batch_size, data_folder, wav_lst, N_snt, wlen, lab_dict, fact_amp):
-  # Initialization of the minibatch (batch_size,[0=>x_t,1=>x_t+N,1=>random_samp])
-  sig_batch = np.zeros([batch_size, wlen])
-  lab_batch = np.zeros(batch_size)
+LABEL_SAME_CLASS = 0
+LABEL_DIFF_CLASS = 1
 
-  snt_id_arr = np.random.randint(N_snt, size=batch_size)
+M = 0.1
 
-  rand_amp_arr = np.random.uniform(1.0 - fact_amp, 1 + fact_amp, batch_size)
+def read_list(file_path):
+  lines = []
+  with open(file_path, 'r') as f:
+    for line in f.read().splitlines():
+      line = line.strip()
+      if line:
+        lines.append(line)
+  print(f'{file_path}: {len(lines)} lines')
+  return lines
 
-  for i in range(batch_size):
-    signal, _ = sf.read(data_folder + wav_lst[snt_id_arr[i]])
 
+def timit_by_speaker(audio_file_list):
+  # train/dr1/mpgh0/si675.wav
+  by_speaker = collections.defaultdict(lambda: [])
+  for audio_file in audio_file_list:
+    speaker = audio_file.split('/')[2]
+    by_speaker[speaker].append(audio_file)
+  by_speaker = dict(by_speaker)
+  return by_speaker
+
+
+class Dataset(IterableDataset):
+
+  def __init__(self, data_folder, by_speaker, wlen, fact_amp, max_samples):
+    self.data_folder = data_folder
+    self.wlen = wlen
+    self.fact_amp = fact_amp
+    self.by_speaker = by_speaker
+    self.all_speakers = list(by_speaker.keys())
+    self.max_samples = max_samples
+    self.num_sampled = 0
+
+  def __iter__(self):
+    return self
+
+  def load_random_chunk(self, file_path):
+    signal, _ = sf.read(os.path.join(self.data_folder, file_path))
+    channels = len(signal.shape)
+    if channels >= 2:
+      assert False
     # accessing to a random chunk
     snt_len = signal.shape[0]
     snt_beg = np.random.randint(snt_len - wlen - 1)
     snt_end = snt_beg + wlen
 
-    channels = len(signal.shape)
-    if channels >= 2:
-      assert False
+    rand_amp = np.random.uniform(1.0 - self.fact_amp, 1 + self.fact_amp)
+    chunk = signal[snt_beg:snt_end] * rand_amp
+    chunk = torch.from_numpy(chunk).float().cuda(device)
+    return chunk
 
-    sig_batch[i, :] = signal[snt_beg:snt_end] * rand_amp_arr[i]
-    lab_batch[i] = lab_dict[wav_lst[snt_id_arr[i]]]
+  def __next__(self):
+    # TODO @incomplete: hard negative mining
+    if self.max_samples is None or self.num_sampled < self.max_samples:
+      self.num_sampled += 1
+      label = np.random.choice([LABEL_SAME_CLASS, LABEL_DIFF_CLASS])
+      if label == LABEL_SAME_CLASS:
+        speaker = random.choice(self.all_speakers)
+        file_a, file_b = random.choices(self.by_speaker[speaker], k=2)
+      elif label == LABEL_DIFF_CLASS:
+        speaker_a = random.choice(self.all_speakers)
+        file_a = random.choice(self.by_speaker[speaker_a])
+        while True:
+          speaker_b = random.choice(self.all_speakers)
+          if speaker_b != speaker_a:
+            break
+        file_b = random.choice(self.by_speaker[speaker_b])
+      else:
+        assert False
 
-  inp = torch.from_numpy(sig_batch).float().cuda(device).contiguous()
-  lab = torch.from_numpy(lab_batch).float().cuda(device).contiguous()
-
-  return inp, lab
+      a = self.load_random_chunk(file_a)
+      b = self.load_random_chunk(file_b)
+      return a, b, label
+    else:
+      raise StopIteration()
 
 
 # Reading cfg file
@@ -59,7 +113,7 @@ tr_lst = options.tr_lst
 te_lst = options.te_lst
 pt_file = options.pt_file
 class_dict_file = options.lab_dict
-data_folder = options.data_folder + '/'
+data_folder = options.data_folder
 output_folder = options.output_folder
 
 # [windowing]
@@ -96,21 +150,18 @@ N_eval_epoch = int(options.N_eval_epoch)
 seed = int(options.seed)
 
 # training list
-wav_lst_tr = ReadList(tr_lst)
-snt_tr = len(wav_lst_tr)
+wav_lst_tr = read_list(tr_lst)
+by_speaker_tr = timit_by_speaker(audio_file_list=wav_lst_tr)
 
 # test list
-wav_lst_te = ReadList(te_lst)
-snt_te = len(wav_lst_te)
+wav_lst_te = read_list(te_lst)
+by_speaker_te = timit_by_speaker(audio_file_list=wav_lst_te)
 
 os.makedirs(output_folder, exist_ok=True)
 
 # setting seed
 torch.manual_seed(seed)
 np.random.seed(seed)
-
-# loss function
-cost = nn.NLLLoss()
 
 # Converting context and shift in samples
 wlen = int(fs * cw_len / 1000.00)
@@ -156,113 +207,86 @@ if pt_file != 'none':
   checkpoint_load = torch.load(pt_file)
   CNN_net.load_state_dict(checkpoint_load['CNN_model_par'])
   DNN1_net.load_state_dict(checkpoint_load['DNN1_model_par'])
-  DNN2_net.load_state_dict(checkpoint_load['DNN2_model_par'])
 
 optimizer_CNN = optim.RMSprop(CNN_net.parameters(), lr=lr, alpha=0.95, eps=1e-8)
 optimizer_DNN1 = optim.RMSprop(DNN1_net.parameters(), lr=lr, alpha=0.95, eps=1e-8)
-optimizer_DNN2 = optim.RMSprop(DNN2_net.parameters(), lr=lr, alpha=0.95, eps=1e-8)
+
+def calc_loss(xs, ys, labels):
+  dw = torch.norm((xs - ys), p=2, dim=1)
+  ls = dw ** 2 / 2
+  ld = torch.max(M - dw, torch.zeros(dw.shape, dtype=torch.float32).cuda(device))
+  ld = ld ** 2 / 2
+  loss = (1 - labels) * ls + labels * ld
+  loss = torch.mean(loss)
+  return loss
 
 print('localtime when starting:', time.localtime())
+train_data_set = Dataset(
+  data_folder=data_folder,
+  by_speaker=by_speaker_tr,
+  wlen=wlen,
+  fact_amp=0.2,
+  max_samples=None)
+train_data_loader = DataLoader(train_data_set, batch_size=batch_size)
+train_data_iter = train_data_loader.__iter__()
 for epoch in range(N_epochs):
   epoch_start = time.monotonic()
   test_flag = 0
   CNN_net.train()
   DNN1_net.train()
-  DNN2_net.train()
 
   loss_sum = 0
-  err_sum = 0
 
   for i in range(N_batches):
-    [inp, lab] = create_batches_rnd(batch_size, data_folder, wav_lst_tr, snt_tr, wlen, lab_dict, 0.2)
-    pout = DNN2_net(DNN1_net(CNN_net(inp)))
+    part_a, part_b, labels = next(train_data_iter)
+    labels = labels.float().cuda()
+    vectors_a = DNN1_net(CNN_net(part_a))
+    vectors_b = DNN1_net(CNN_net(part_b))
 
-    pred = torch.max(pout, dim=1)[1]
-    loss = cost(pout, lab.long())
-    err = torch.mean((pred != lab.long()).float())
+    loss = calc_loss(vectors_a, vectors_b, labels)
 
     optimizer_CNN.zero_grad()
     optimizer_DNN1.zero_grad()
-    optimizer_DNN2.zero_grad()
-
     loss.backward()
     optimizer_CNN.step()
     optimizer_DNN1.step()
-    optimizer_DNN2.step()
 
     loss_sum = loss_sum + loss.detach()
-    err_sum = err_sum + err.detach()
 
-  loss_tot = loss_sum / N_batches
-  err_tot = err_sum / N_batches
+  loss_tr = loss_sum / N_batches
 
-  # Full Validation  new
+  stat = f'epoch={epoch:03d} time={int(time.monotonic() - epoch_start)} loss_tr={loss_tr:.4f}'
+
   if epoch % N_eval_epoch == 0:
 
     CNN_net.eval()
     DNN1_net.eval()
-    DNN2_net.eval()
-    test_flag = 1
     loss_sum = 0
-    err_sum = 0
-    err_sum_snt = 0
 
     with torch.no_grad():
-      for i in range(snt_te):
-        signal, _ = sf.read(data_folder + wav_lst_te[i])
-        lab_batch = lab_dict[wav_lst_te[i]]
-
-        # 1 2 3 4 5 6 7 8 9 10 11 12 13
-        # stride = 3
-        # window = 5
-        # we should have (13 - 5) // 3 + 1 = 3 examples
-        # for we have (13 - 5) segments, each segment corresponds to a left end point,
-        # and each left point corresponds to one example,
-        # plus the one at the end
-        # 1 2 3 4 5
-        # 4 5 6 7 8
-        # 7 8 9 10 11
-
-        N_fr_actual = 0
-        N_fr = (signal.shape[0] - wlen) // wshift + 1
-        sig_arr = torch.zeros([N_fr, wlen]).float().cuda(device).contiguous()
-        lab = (torch.zeros(N_fr) + lab_batch).cuda(device).contiguous().long()
-        for i_sig, beg_samp in enumerate(range(0, signal.shape[0], wshift)):
-          end_samp = beg_samp + wlen
-          if end_samp > signal.shape[0]:
-            break
-          else:
-            sig_arr[i_sig] = signal[beg_samp:end_samp]
-            N_fr_actual += 1
-        assert N_fr_actual == N_fr
-        pout = DNN2_net(DNN1_net(CNN_net(sig_arr)))
-
-        pred = torch.max(pout, dim=1)[1]
-        loss = cost(pout, lab.long())
-        err = torch.mean((pred != lab.long()).float())
-
-        [val, best_class] = torch.max(torch.sum(pout, dim=0), 0)
-        err_sum_snt = err_sum_snt + (best_class != lab[0]).float()
-
+      samples_to_eval = (len(by_speaker_te) * 2 // batch_size + 1) * batch_size
+      eval_data_set = Dataset(
+        data_folder=data_folder,
+        by_speaker=by_speaker_te,
+        wlen=wlen,
+        fact_amp=0,
+        max_samples=samples_to_eval)
+      eval_data_loader = DataLoader(eval_data_set, batch_size=batch_size)
+      for part_a, part_b, labels in eval_data_loader:
+        labels = labels.float().cuda()
+        vectors_a = DNN1_net(CNN_net(part_a))
+        vectors_b = DNN1_net(CNN_net(part_b))
+        loss = calc_loss(vectors_a, vectors_b, labels)
         loss_sum = loss_sum + loss.detach()
-        err_sum = err_sum + err.detach()
+      loss_te = loss_sum / (samples_to_eval / batch_size)
 
-      err_tot_dev_snt = err_sum_snt / snt_te
-      loss_tot_dev = loss_sum / snt_te
-      err_tot_dev = err_sum / snt_te
+    stat += f' loss_te={loss_te:.4f}'
 
-    print("epoch %i, loss_tr=%f err_tr=%f loss_te=%f err_te=%f err_te_snt=%f time=%f" % (
-      epoch, loss_tot, err_tot, loss_tot_dev, err_tot_dev, err_tot_dev_snt, time.monotonic() - epoch_start))
-
-    with open(output_folder + "/res.res", "a") as res_file:
-      res_file.write("epoch %i, loss_tr=%f err_tr=%f loss_te=%f err_te=%f err_te_snt=%f time=%f\n" % (
-        epoch, loss_tot, err_tot, loss_tot_dev, err_tot_dev, err_tot_dev_snt, time.monotonic() - epoch_start))
+    with open(os.path.join(output_folder, 'res.res'), 'a') as res_file:
+      res_file.write(stat + '\n')
 
     checkpoint = {'CNN_model_par': CNN_net.state_dict(),
-                  'DNN1_model_par': DNN1_net.state_dict(),
-                  'DNN2_model_par': DNN2_net.state_dict()}
-    torch.save(checkpoint, output_folder + f'/model_raw_{epoch}.pkl')
+                  'DNN1_model_par': DNN1_net.state_dict()}
+    torch.save(checkpoint, os.path.join(output_folder, f'model_raw_{epoch:03d}.pkl'))
 
-  else:
-    print("epoch %i, loss_tr=%f err_tr=%f time=%f" % (
-      epoch, loss_tot, err_tot, time.monotonic() - epoch_start))
+  print(stat)
